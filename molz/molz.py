652cb02152d60661228977962cd8fac74c706133
Liam Wilbraham <liamwilbraham@gmail.com>
2020-12-06 19:07:48 +0000

    bulk of first draft, tests, basic plotting

--- molz/molz.py
+++ molz/molz.py
@@ -1,17 +1,27 @@
+import tqdm
 import numpy as np
 import pandas as pd
 from rdkit import Chem
 from rdkit.Chem import AllChem
+import matplotlib.pyplot as plt
 
 
 class ZScorer:
 
-    def __init__(self, fp_rad=3, fp_bits=4096):
+    def __init__(self, data, fp_rad=3, fp_bits=4096, from_preprocessed_pickle=False):
+        # fingerprint params
         self.fp_rad = fp_rad
         self.fp_bits = fp_bits
 
         # zscores for fragments will be stored here
         self.zscores = {}
+
+        if from_preprocessed_pickle:
+            # load preprocessed data from pickle
+            self._load_processed_data(data)
+        else:
+            # load data and compute rdkit mol objs
+            self._load_molecule_property_data(data)
 
     def score_fragments(self, prop, prop_range, fragment_smiles=None):
         if fragment_smiles:
@@ -21,13 +31,33 @@ class ZScorer:
             self._compute_morgan_frags()
             fragments = list(range(self.fp_bits))
 
-        for frag_id in fragments:
+        for frag_id in tqdm.tqdm(fragments, desc='Computing fragment z-scores'):
             self.zscores[frag_id] = self._compute_frag_zscore(
                 frag_id, prop, prop_range
             )
 
     def plot(self):
-        pass
+        x, y = [], []
+        for frag, zscore in self.zscores.items():
+            x.append(frag)
+            y.append(zscore)
+
+        import matplotlib.cm as cm
+        from matplotlib.colors import Normalize
+
+        my_cmap = cm.get_cmap('RdYlGn')
+        my_norm = Normalize(vmin=min(y), vmax=max(y))
+
+        fix, ax = plt.subplots(1, 1)
+        ax.barh(x, y, color=my_cmap(my_norm(y)))
+
+        plt.savefig('fig.png')
+
+    def pickle_processed_data(self, picklename):
+        self.data.to_pickle(picklename)
+
+    def _load_processed_data(self, picklename):
+        self.data = pd.read_pickle(picklename)
 
     def _load_molecule_property_data(self, datafile):
         self.data = pd.read_csv(datafile)
@@ -37,26 +67,22 @@ class ZScorer:
         self.data['mol'] = self.data.smiles.apply(Chem.MolFromSmiles)
 
     def _compute_morgan_fps(self):
-        self.data['fp'] = self.data.mol.apply(
-            AllChem.GetMorganFingerprintAsBitVect, args=(
-                self.fp_rad, self.fp_bits)
-        )
+        self.fps = np.vstack([
+            AllChem.GetMorganFingerprintAsBitVect(mol, self.fp_rad, self.fp_bits)
+            for mol in self.data.mol
+        ])
 
     def _compute_morgan_frags(self):
         self._compute_morgan_fps()
-        fp_array = np.vstack([arr for arr in self.data.fp])
-        for i, col in enumerate(fp_array.T):
-            self.data[i] = col
-        self.data = self.data.drop(['fp'], axis=1)
+        np_df = pd.DataFrame(self.fps, columns=[i for i in range(self.fp_bits)])
+        self.data = pd.concat([self.data, np_df], axis=1)
 
     def _compute_user_frags(self, frags):
         frags = [(f, Chem.MolFromSmiles(f)) for f in frags]
         for smiles, mol in frags:
-            self.data[smiles] = self.data.mol.apply(
-                self._compute_frag_matches, args=(mol,)
-            )
+            self.data[smiles] = self.data.mol.apply(self._compute_user_frag_matches, args=(mol,))
 
-    def _compute_frag_matches(self, mol, pattern):
+    def _compute_user_frag_matches(self, mol, pattern):
         if mol.HasSubstructMatch(pattern):
             return 1
         return 0
@@ -68,24 +94,29 @@ class ZScorer:
         )
         subpop = self.data[subpop_range]
 
-        print('')
-        print(self.data)
-        print(subpop)
-
         pop_total = self.data[frag_id].sum()
-        subpop_total = subpop[frag_id].sum()
+        selection_total = subpop[frag_id].sum()
 
-        print(pop_total)
-        print(subpop_total)
+        N = len(self.data)  # total in population
+        n = len(subpop)  # total in selection
+        k = pop_total  # num in population with fragment
+        x = selection_total  # num in selection with fragment
 
-        N = len(self.data)
-        k = pop_total
-        n = len(subpop)
-        x = subpop_total
+        # mean and variance of hypergeometric dist.
         mean = n * k / N
-        var = n * k * (N - k) * (N - n) / (N**2 * (N - 1))
+        var = n * k * (N - k) * (N - n) / (N**2 * (N - 1)) + 1e-30
 
+        # compute zscore from the above
         zscore = (x - mean) / var
-        print(zscore)
 
+        # print('N:', N)
+        # print('n:', n)
+        # print('k:', k)
+        # print('x:', x)
+        # print('mean:', mean)
+        # print('var:', var)
+        # print('zscore:', zscore)
         return zscore
+
+    def _get_fragment_images_for_plotting(self, fragment_ids):
+        pass
--- tests/test_molz.py
+++ tests/test_molz.py
@@ -1,38 +1,82 @@
+from pytest import approx
 from molz import ZScorer
 
 
-# def test_load_molecule_property_data():
-#     scorer = ZScorer()
-#     scorer._load_molecule_property_data('tests/data.csv')
-#     assert 'mol' in scorer.data.columns
-#     assert len(scorer.data) == 2
+def test_load_molecule_property_data():
+    scorer = ZScorer('tests/data.csv')
+    assert 'mol' in scorer.data.columns
+    assert len(scorer.data) == 2
 
 
-# def test_compute_user_frags():
-#     scorer = ZScorer()
-#     scorer._load_molecule_property_data('tests/data.csv')
+def test_compute_user_frags():
+    scorer = ZScorer('tests/data.csv')
+    scorer._compute_user_frags(['c1ccccc1', 'CCCC'])
 
-#     scorer._compute_user_frags(['c1ccccc1', 'CCCC'])
-
-#     assert scorer.data['c1ccccc1'].iloc[0] == 1
-#     assert scorer.data['c1ccccc1'].iloc[1] == 0
-#     assert scorer.data['CCCC'].iloc[0] == 0
-#     assert scorer.data['CCCC'].iloc[1] == 1
+    assert scorer.data['c1ccccc1'].iloc[0] == 1
+    assert scorer.data['c1ccccc1'].iloc[1] == 0
+    assert scorer.data['CCCC'].iloc[0] == 0
+    assert scorer.data['CCCC'].iloc[1] == 1
 
 
-# def test_compute_morgan_frags():
-#     scorer = ZScorer(fp_rad=0, fp_bits=8)
-#     scorer._load_molecule_property_data('tests/data.csv')
+def test_compute_morgan_frags():
+    scorer = ZScorer('tests/data.csv', fp_rad=0, fp_bits=8)
+    scorer._compute_morgan_frags()
 
-#     scorer._compute_morgan_frags()
-
-#     assert len(scorer.data.columns) == 11  # each frag col plus originals
-#     assert scorer.data[0].iloc[0] == 0
-#     assert scorer.data[0].iloc[1] == 1
+    assert len(scorer.data.columns) == 11  # each frag col plus originals
+    assert scorer.data[0].iloc[0] == 0
+    assert scorer.data[0].iloc[1] == 1
 
 
-def test_score_fragments():
-    scorer = ZScorer()
-    scorer._load_molecule_property_data('tests/data.csv')
+def test_score_fragments_user_defined():
+    scorer = ZScorer('tests/data.csv')
 
-    scorer.score_fragments('molwt', [60, 100], fragment_smiles=['c1ccccc1'])
+    scorer.score_fragments(
+        'molwt', [60, 100], fragment_smiles=['c1ccccc1', 'O', 'CCC']
+    )
+
+    assert scorer.zscores['c1ccccc1'] == approx(2.0)
+    assert scorer.zscores['O'] == approx(0.0)
+    assert scorer.zscores['CCC'] == approx(-2.0)
+    # print(scorer.zscores)
+
+
+def test_score_fragments_morgan():
+    scorer = ZScorer('tests/data.csv', fp_bits=8, fp_rad=0)
+
+    scorer.score_fragments(
+        'molwt', [60, 100]
+    )
+
+    assert scorer.zscores[0] == approx(-2.0)
+    for i in range(1, 8):
+        assert scorer.zscores[i] == approx(0.0)
+
+
+def test_pickle_processed_data():
+    scorer = ZScorer('tests/data.csv')
+
+    scorer.score_fragments(
+        'molwt', [60, 100], fragment_smiles=['c1ccccc1', 'O', 'CCC']
+    )
+
+    scorer.pickle_processed_data('pytestpickle.pkl')
+
+    # set data attrib to none to ensure load works as expected
+    scorer.data = None
+
+    scorer._load_processed_data('pytestpickle.pkl')
+
+    assert len(scorer.data) == 2
+
+
+def test_plot():
+    scorer = ZScorer('tests/data.csv')
+
+    scorer.score_fragments(
+        'molwt', [60, 100], fragment_smiles=['c1ccccc1', 'O', 'CCC']
+    )
+
+    scorer.plot()
+
+
+test_plot()
