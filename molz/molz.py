6dfb80d301e949ce6ef7e8e411121a07fd622470
Ben Saunders <54156568+benedictsaunders@users.noreply.github.com>
2021-05-19 15:14:12 +0100

    Added multiple conditions functionality

--- molz/molz.py
+++ molz/molz.py
@@ -1,8 +1,12 @@
 from typing import Tuple, List, Union
 
+from . import utils
+
 import tqdm
 import numpy as np
+import scipy.stats as stats
 import pandas as pd
+from pandasql import sqldf
 
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
@@ -27,16 +31,17 @@ class ZScorer:
     fragment. Fragments can either be user-defined, or auto-generated using Morgan circular
     fingerprints.
     """
-    FP_TYPES = ['rdkit', 'morgan']
+
+    FP_TYPES = ["rdkit", "morgan"]
 
     def __init__(
         self,
-        data: str,
+        datafile: str,
         fp_rad: int = 3,
         fp_bits: int = 4096,
-        fp_type: str = 'morgan',
+        fp_type: str = "morgan",
         from_preprocessed_pickle: str = None,
-        hide_progress: bool = False
+        hide_progress: bool = False,
     ) -> None:
         """Init method for ZScorer.
 
@@ -53,37 +58,35 @@ class ZScorer:
                 dealing with large datasets Defaults to None.
             hide_progress (bool, optional): Supress progress bar outputs. Defaults to False.
         """
+        self.datafile = datafile
+
         # fingerprint params
         self.fp_rad = fp_rad
         self.fp_bits = fp_bits
         self.fp_type = fp_type
 
         if fp_type not in self.FP_TYPES:
-            raise Exception('Fingerprint type not supported.')
+            raise Exception("Fingerprint type not supported.")
 
         self.prog = hide_progress
         self.user_frags = False
         self.data = None
         self.fps = None
-        self.prop_range = None
-        self.prop = None
+        self.props = None
+        self.prop_ranges = None
+        self.from_preprocessed_pickle = from_preprocessed_pickle
+        #
 
+        """
+        Moved the processing to score_fragments, so we only import the
+        necessary columns, as defined with the properties
+        """
         # zscores for fragments will be stored here
         self.zscores = {}
 
-        if from_preprocessed_pickle:
-            # load preprocessed data from pickle
-            self._load_processed_data(from_preprocessed_pickle)
-            self.use_preprocessed = True
-        else:
-            # load data and compute rdkit mol objs
-            self._load_molecule_property_data(data)
-            self.use_preprocessed = False
-
     def score_fragments(
         self,
-        prop: str,
-        prop_range: List[float],
+        properties: List[Tuple[str, Tuple[float, float]]],
         fragment_smarts: List[str] = None,
     ) -> None:
         """Compute zscores for user-defined or auto-generated fragments.
@@ -95,8 +98,26 @@ class ZScorer:
             fragment_smarts (List[str], optional): User-defined fragments. Defaults to None,
                 in which case fragments are auto-generated.
         """
-        self.prop_range = prop_range
-        self.prop = prop
+        props = []
+        prop_ranges = []
+        for prop in properties:
+            props.append(prop[0])
+            prop_ranges.append(prop[1])
+        self.prop_ranges = prop_ranges
+        self.props = props
+
+        cols = ["smiles"] + props
+
+        # from initialisation function
+
+        if self.from_preprocessed_pickle:
+            # load preprocessed data from pickle
+            self._load_processed_data(self.from_preprocessed_pickle)
+            self.use_preprocessed = True
+        else:
+            # load data and compute rdkit mol objs
+            self._load_molecule_property_data(self.datafile, cols)
+            self.use_preprocessed = False
 
         # user-defined fragments
         if fragment_smarts:
@@ -111,20 +132,16 @@ class ZScorer:
                 self._generate_df_with_fragments()
             fragments = list(range(self.fp_bits))
 
-        sample_range = (
-            (self.data[prop] >= prop_range[0])
-            & (self.data[prop] <= prop_range[1])
-        )
-
-        # get sample in specified property range
-        sample = self.data[sample_range]
+        sample = self.getSample()
 
         # compute total number of times each fragment appears in data
         totals = [self.data[frag_id].sum() for frag_id in fragments]
 
         # compute and store fragment zscores
         i = 0
-        for frag_id in tqdm.tqdm(fragments, desc='Computing fragment z-scores', disable=self.prog):
+        for frag_id in tqdm.tqdm(
+            fragments, desc="Computing fragment z-scores", disable=self.prog
+        ):
             self.zscores[frag_id] = self._compute_frag_zscore(
                 frag_id, sample, totals[i]
             )
@@ -136,7 +153,7 @@ class ZScorer:
         save_to: str = None,
         figsize: Tuple[int, int] = (8, 4),
         top_only: bool = False,
-        log_y: bool = False
+        log_y: bool = False,
     ):
         """Create a bar plot of top and bottom k zscoring fragments.
 
@@ -155,13 +172,19 @@ class ZScorer:
             frag_ids, frag_scores = frag_ids[k:], frag_scores[k:]
 
         # create color gradient map
-        my_cmap = cm.get_cmap('RdYlGn')
+        my_cmap = cm.get_cmap("RdYlGn")
         my_norm = Normalize(vmin=-max(frag_scores), vmax=max(frag_scores))
 
         # make plot
         fig, axis = plt.subplots(1, 1, figsize=figsize)
-        axis.bar(frag_ids, frag_scores, color=my_cmap(my_norm(frag_scores)), width=0.4, log=log_y)
-        axis.set_ylabel('z-score (std. dev.)')
+        axis.bar(
+            frag_ids,
+            frag_scores,
+            color=my_cmap(my_norm(frag_scores)),
+            width=0.4,
+            log=log_y,
+        )
+        axis.set_ylabel("z-score (std. dev.)")
 
         plt.xticks(rotation=90)
         plt.tight_layout()
@@ -171,7 +194,9 @@ class ZScorer:
 
         plt.show()
 
-    def draw_fragment(self, fragment_id: Union[str, int], show_zscore: bool = True) -> str:
+    def draw_fragment(
+        self, fragment_id: Union[str, int], show_zscore: bool = True
+    ) -> str:
         """Draw a specified fragmnet.
 
         Args:
@@ -184,36 +209,29 @@ class ZScorer:
         """
 
         # images will be annotated with zscore
-        legend = f'zscore = {self.zscores[fragment_id]:.2f}' if show_zscore else ''
+        legend = f"zscore = {self.zscores[fragment_id]:.2f}" if show_zscore else ""
 
         # handle drawing of user-defined fragments
         if self.user_frags:
             mol = Chem.MolFromSmarts(fragment_id)
             return Draw.MolsToGridImage(
-                [mol],
-                molsPerRow=1,
-                subImgSize=(200, 200),
-                legends=[legend]
+                [mol], molsPerRow=1, subImgSize=(200, 200), legends=[legend]
             )
 
         # handle drawing of auto-generated fragments
         mol = self._get_mol_with_frag(fragment_id)
 
         bit_info = {}
-        if self.fp_type == 'morgan':
+        if self.fp_type == "morgan":
             _ = rdMolDescriptors.GetMorganFingerprintAsBitVect(
                 mol, radius=self.fp_rad, nBits=self.fp_bits, bitInfo=bit_info
             )
 
             return Draw.DrawMorganBit(
-                mol,
-                fragment_id,
-                bit_info,
-                useSVG=True,
-                legend=legend
+                mol, fragment_id, bit_info, useSVG=True, legend=legend
             )
 
-        if self.fp_type == 'rdkit':
+        if self.fp_type == "rdkit":
             _ = Chem.RDKFingerprint(
                 mol,
                 minPath=self.fp_rad,
@@ -223,11 +241,7 @@ class ZScorer:
             )
 
             return Draw.DrawRDKitBit(
-                mol,
-                fragment_id,
-                bit_info,
-                useSVG=True,
-                legend=legend
+                mol, fragment_id, bit_info, useSVG=True, legend=legend
             )
 
     def pickle_processed_data(self, picklename: str) -> None:
@@ -238,6 +252,65 @@ class ZScorer:
         """
         self.data.to_pickle(picklename)
 
+    def _load_processed_data(self, picklename: str) -> None:
+        """Load previously pickled dataframe.
+
+        Args:
+            picklename (str): Path to preprocessed data.
+        """
+        self.data = pd.read_pickle(picklename)
+
+    def _load_molecule_property_data(self, datafile: str, cols: List[str]) -> None:
+        """Load data from .CSV.
+
+        Args:
+            datafile (str): Path to .CSV.
+        """
+        self.data = pd.read_csv(datafile, usecols=cols, low_memory=True)
+        self.data.insert(0, "ID", range(0, len(self.data)))
+        self._compute_mols_from_smiles()
+
+    def getSample(self):
+        """
+        Querying the data and returns a sample that meets specificied criteria
+
+        Props and prop_ranges are converted into a SQL query and executed on a temporary
+        dataframe, which lacks the fingerprint bit. The sample from the temporary df is
+        then joined with the main data, using the ID field in order to get the fp bits back.
+
+        We need a temporary classless df to query from, which is deleted later. For large
+        datasets, this could cause memory issues, so again, might be best to use a sqlite db
+        and query that directly?
+        """
+
+        params_list = []
+        tmp_df = self.data[["ID"] + list(self.props)].copy()
+
+        for i, prop in enumerate(self.props):
+            params_list.append(
+                f"{prop} >= {self.prop_ranges[i][0]} AND {prop} <= {self.prop_ranges[i][1]}"
+            )
+        params = " AND ".join(params_list)
+        sql = "SELECT * FROM tmp_df WHERE " + params + ";"
+
+        # get sample in specified property range
+        queried = sqldf(sql)
+        sample = queried.merge(
+            self.data,
+            how="left",
+            on="ID",
+            suffixes=("", "__y__"),
+        )
+        sample.drop(
+            sample.filter(regex="__y__$").columns.tolist(),
+            axis=1,
+            inplace=True,
+        )
+        del tmp_df
+        percent = 100.0 * float(len(sample) / len(self.data))
+        print(f"\nSample constitutes {round(percent, 3)}% of the total data.\n")
+        return sample
+
     def _get_mol_with_frag(self, frag_id: Union[str, int]) -> Chem.Mol:
         """Given a fragment id, return a mol containing that fragment.
 
@@ -247,16 +320,8 @@ class ZScorer:
         Returns:
             Chem.Mol: RDKit mol object of mol containing fragment.
         """
-        if self.prop_range:
-            sample_range = (
-                (self.data[self.prop] >= self.prop_range[0])
-                & (self.data[self.prop] <= self.prop_range[1])
-            )
-
-            # get sample in specified property range
-            sample = self.data[sample_range]
-        else:
-            sample = self.data
+        if self.prop_ranges:
+            sample = self.getSample()
 
         # if fragment not present in range, draw mol from all data
         if len(sample[sample[int(frag_id)] == 1]) == 0:
@@ -286,43 +351,28 @@ class ZScorer:
 
         return frag_ids, frag_scores
 
-    def _load_processed_data(self, picklename: str) -> None:
-        """Load previously pickled dataframe.
-
-        Args:
-            picklename (str): Path to preprocessed data.
-        """
-        self.data = pd.read_pickle(picklename)
-
-    def _load_molecule_property_data(self, datafile: str) -> None:
-        """Load data from .CSV.
-
-        Args:
-            datafile (str): Path to .CSV.
-        """
-        self.data = pd.read_csv(datafile)
-        self._compute_mols_from_smiles()
-
     def _compute_mols_from_smiles(self) -> None:
-        """Given a list of smiles, compute the RDKit mol objects.
-        """
+        """Given a list of smiles, compute the RDKit mol objects."""
         mols = []
-        for smi in tqdm.tqdm(self.data.smiles, desc='Processing SMILES', disable=self.prog):
+        for smi in tqdm.tqdm(
+            self.data.smiles, desc="Processing SMILES", disable=self.prog
+        ):
             mols.append(Chem.MolFromSmiles(smi))
-        self.data['mol'] = mols
+        self.data["mol"] = mols
 
     def _compute_fps(self) -> None:
-        """Compute a numpy array of Morgan fingerprint vectors.
-        """
+        """Compute a numpy array of Morgan fingerprint vectors."""
         fp_vects = []
-        for mol in tqdm.tqdm(self.data.mol, desc='Computing fingerprints', disable=self.prog):
+        for mol in tqdm.tqdm(
+            self.data.mol, desc="Computing fingerprints", disable=self.prog
+        ):
 
-            if self.fp_type == 'morgan':
+            if self.fp_type == "morgan":
                 fp_vect = rdMolDescriptors.GetMorganFingerprintAsBitVect(
                     mol, self.fp_rad, self.fp_bits
                 )
 
-            if self.fp_type == 'rdkit':
+            if self.fp_type == "rdkit":
                 fp_vect = Chem.RDKFingerprint(
                     mol,
                     minPath=self.fp_rad,
@@ -330,7 +380,7 @@ class ZScorer:
                     fpSize=self.fp_bits,
                 )
 
-            array = np.zeros((0, ), dtype=np.int8)
+            array = np.zeros((0,), dtype=np.int8)
             DataStructs.ConvertToNumpyArray(fp_vect, array)
             fp_vects.append(array)
 
@@ -339,8 +389,7 @@ class ZScorer:
             self.fps[i, :] = fp_vect
 
     def _generate_df_with_fragments(self) -> None:
-        """Place morgan fingerprints vectors into dataframe.
-        """
+        """Place morgan fingerprints vectors into dataframe."""
         self._compute_fps()
         np_df = pd.DataFrame(self.fps, columns=list(range(self.fp_bits)))
         self.data = pd.concat([self.data, np_df], axis=1)
@@ -353,7 +402,9 @@ class ZScorer:
         """
         frags = [(f, Chem.MolFromSmarts(f)) for f in frags]
         for smiles, mol in frags:
-            self.data[smiles] = self.data.mol.apply(self._compute_user_frag_matches, args=(mol,))
+            self.data[smiles] = self.data.mol.apply(
+                self._compute_user_frag_matches, args=(mol,)
+            )
 
     def _compute_user_frag_matches(self, mol: Chem.Mol, pattern: Chem.Mol) -> int:
         """Check if molecule contains user-defined fragment.
@@ -370,10 +421,7 @@ class ZScorer:
         return 0
 
     def _compute_frag_zscore(
-        self,
-        frag_id: Union[str, int],
-        subpop: pd.DataFrame,
-        total: int
+        self, frag_id: Union[str, int], subpop: pd.DataFrame, total: int
     ) -> float:
         """Compute zscores for a given fragment.
 
@@ -394,9 +442,11 @@ class ZScorer:
         k = pop_total  # num in population with fragment
         x = selection_total  # num in selection with fragment
 
-        # mean and variance of hypergeometric dist.
-        mean = n * k / N
-        var = n * k * (N - k) * (N - n) / (N**2 * (N - 1)) + 1e-30
+        # Using sp just so it's easy to switch functions if need be. Granted it's a little
+        # slower than previous but easy enough to switch back
+        mean = stats.hypergeom.mean(N, n, k)
+        var = stats.hypergeom.var(N, n, k) + 1e-30
 
-        # compute zscore from the above
-        return (x - mean) / var
+        z = (x - mean) / var
+
+        return z
